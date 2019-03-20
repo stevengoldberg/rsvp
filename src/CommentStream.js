a08819624ea9dd60ca11c1de41167650f92accee
Steve Goldberg <steve.schnee@gmail.com>
2019-03-20 16:11:44 -0700

    Add Jump to latest button

--- src/CommentStream.js
+++ src/CommentStream.js
@@ -11,11 +11,18 @@ import styles from './CommentStream.module.css'
 const CommentStream = () => {
   const { messageList } = useStream('comments')
   const listRef = useRef()
+
   return (
     <div className={styles.root}>
       <div className={styles.header}>
         <div className={styles.title}>Comments</div>
-        <button onClick={() => {listRef.current.scrollToRow(messageList.length - 1)}}>Jump to newest</button>
+        <button
+          onClick={() => {
+            listRef.current.scrollToRow(messageList.length - 1)
+          }}
+        >
+          Jump to latest
+        </button>
       </div>
       <div className={styles.list}>
         <AutoSizer>
@@ -26,7 +33,7 @@ const CommentStream = () => {
               containerHeight={height}
               containerWidth={width}
               RenderComponent={Comment}
-              getListRef={(ref) => {
+              getListRef={ref => {
                 listRef.current = ref
               }}
             />
