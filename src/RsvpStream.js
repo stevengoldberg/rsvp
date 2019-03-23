268a22313911039e22fec1f02b038aaf682e0980
Steve Goldberg <steve.schnee@gmail.com>
2019-03-22 20:28:00 -0700

    Render RSVP members

--- src/RsvpStream.js
+++ src/RsvpStream.js
@@ -5,19 +5,20 @@ import {
   ZoomableGroup,
   Geographies,
   Geography,
-  Annotations,
-  Annotation,
+  Markers,
+  Marker,
 } from 'react-simple-maps'
 import { AutoSizer } from 'react-virtualized'
 
 import useStream from './useStream'
 
-import styles from './RsvpStream.module.css'
+import styles from './RsvpStream.module.scss'
 
 const CHICAGO = [-87, 41]
+const LAND_COLORS = ['#09BC8A', '#92AA83', '#79B473', '#86BAA1', '#A0E8AF']
 
 const RsvpStream = () => {
-  const { messageList } = useStream('rsvp', 20)
+  const { messageList } = useStream('rsvp', 25)
   return (
     <div className={styles.mapContainer}>
       <AutoSizer>
@@ -28,25 +29,30 @@ const RsvpStream = () => {
                 geography={`${process.env.PUBLIC_URL}/world-50m.json`}
               >
                 {(geographies, projection) =>
-                  geographies.map((geography, i) => (
-                    <Geography
-                      key={`geography-${i}`}
-                      cacheId={`geography-${i}`}
-                      geography={geography}
-                      projection={projection}
-                      style={{
-                        default: {
-                          fill: 'gray',
-                          stroke: '#FFF',
-                          strokeWidth: 0.5,
-                          outline: 'none',
-                        },
-                      }}
-                    />
-                  ))
+                  geographies.map((geography, i) => {
+                    const style = {
+                      fill: LAND_COLORS[_.random(0, LAND_COLORS.length - 1)],
+                      stroke: '#FFF',
+                      strokeWidth: 0.5,
+                      outline: 'none',
+                    }
+                    return (
+                      <Geography
+                        key={`geography-${i}`}
+                        cacheId={`geography-${i}`}
+                        geography={geography}
+                        projection={projection}
+                        style={{
+                          default: style,
+                          hover: style,
+                          pressed: style,
+                        }}
+                      />
+                    )
+                  })
                 }
               </Geographies>
-              <Annotations>
+              <Markers>
                 {messageList.map(({ message: rsvp, id }) => {
                   const coordinates = []
                   if (rsvp.venue) {
@@ -55,20 +61,23 @@ const RsvpStream = () => {
                     coordinates.push(rsvp.group.group_lon, rsvp.group.group_lat)
                   }
                   return (
-                    <Annotation
-                      key={id}
-                      subject={coordinates}
-                      dx={40}
-                      dy={20}
-                      strokeWidth={0.5}
-                    >
-                      <text className={styles.mapLabel} alignment-baseline="central" dx="1px">
-                        {_.get(rsvp, 'member.member_name')}
-                      </text>
-                    </Annotation>
+                    <Marker key={id} marker={{ coordinates }}>
+                      <defs>
+                        <clipPath id="circleView">
+                          <circle cx="40" cy="20" r="20" fill="#FFFFFF" />
+                        </clipPath>
+                      </defs>
+                      <image
+                        clipPath="url(#circleView)"
+                        xlinkHref={_.get(rsvp, 'member.photo')}
+                        width={80}
+                        height={80}
+                        style={{ background: 'white' }}
+                      />
+                    </Marker>
                   )
                 })}
-              </Annotations>
+              </Markers>
             </ZoomableGroup>
           </ComposableMap>
         )}
