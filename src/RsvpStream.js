45d51ecdfb7d93b153716f0f706ac9b74812c63b
Steve Goldberg <steve.schnee@gmail.com>
2019-03-22 22:19:02 -0700

    Add snap to latest RSVPs

--- src/RsvpStream.js
+++ src/RsvpStream.js
@@ -1,85 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import _ from 'lodash'
-import {
-  ComposableMap,
-  ZoomableGroup,
-  Geographies,
-  Geography,
-  Markers,
-  Marker,
-} from 'react-simple-maps'
-import { AutoSizer } from 'react-virtualized'
 
+import { AutoSizer } from 'react-virtualized'
+import { Motion, spring } from 'react-motion'
+
+import WorldMap from './WorldMap'
 import useStream from './useStream'
+
+import { getCoordinates } from './WorldMap'
 
 import styles from './RsvpStream.module.scss'
 
 const CHICAGO = [-87, 41]
-const LAND_COLORS = ['#09BC8A', '#92AA83', '#79B473', '#86BAA1', '#A0E8AF']
 
 const RsvpStream = () => {
   const { messageList } = useStream('rsvp', 25)
+  const [mapCenter, setMapCenter] = useState(CHICAGO)
+  const [snapToEvent, setSnapToEvent] = useState(false)
+  const snapNext = useRef(false)
+
+  useEffect(() => {
+    const latestMessage = _.takeRight(messageList)[0]
+    if (!latestMessage || !snapToEvent) return
+    if (!snapNext.current) {
+      snapNext.current = true
+      return
+    }
+    setMapCenter(getCoordinates(latestMessage.message))
+    snapNext.current = false
+  }, [messageList])
   return (
     <div className={styles.mapContainer}>
+      <div className={styles.title}>
+        RSVP
+        <div className={styles.toggle}>
+          <input
+            type="checkbox"
+            value={snapToEvent}
+            onChange={() => setSnapToEvent(prevValue => !prevValue)}
+          />
+          Snap to latest
+        </div>
+      </div>
       <AutoSizer>
         {({ height, width }) => (
-          <ComposableMap height={height} width={width} projection="miller">
-            <ZoomableGroup zoom={4} center={CHICAGO}>
-              <Geographies
-                geography={`${process.env.PUBLIC_URL}/world-50m.json`}
-              >
-                {(geographies, projection) =>
-                  geographies.map((geography, i) => {
-                    const style = {
-                      fill: LAND_COLORS[_.random(0, LAND_COLORS.length - 1)],
-                      stroke: '#FFF',
-                      strokeWidth: 0.5,
-                      outline: 'none',
-                    }
-                    return (
-                      <Geography
-                        key={`geography-${i}`}
-                        cacheId={`geography-${i}`}
-                        geography={geography}
-                        projection={projection}
-                        style={{
-                          default: style,
-                          hover: style,
-                          pressed: style,
-                        }}
-                      />
-                    )
-                  })
-                }
-              </Geographies>
-              <Markers>
-                {messageList.map(({ message: rsvp, id }) => {
-                  const coordinates = []
-                  if (rsvp.venue) {
-                    coordinates.push(rsvp.venue.lon, rsvp.venue.lat)
-                  } else {
-                    coordinates.push(rsvp.group.group_lon, rsvp.group.group_lat)
-                  }
-                  return (
-                    <Marker key={id} marker={{ coordinates }}>
-                      <defs>
-                        <clipPath id="circleView">
-                          <circle cx="40" cy="20" r="20" fill="#FFFFFF" />
-                        </clipPath>
-                      </defs>
-                      <image
-                        clipPath="url(#circleView)"
-                        xlinkHref={_.get(rsvp, 'member.photo')}
-                        width={80}
-                        height={80}
-                        style={{ background: 'white' }}
-                      />
-                    </Marker>
-                  )
-                })}
-              </Markers>
-            </ZoomableGroup>
-          </ComposableMap>
+          <Motion
+            defaultStyle={{
+              x: mapCenter[0],
+              y: mapCenter[1],
+            }}
+            style={{
+              x: spring(mapCenter[0], { stiffness: 210, damping: 20 }),
+              y: spring(mapCenter[1], { stiffness: 210, damping: 20 }),
+            }}
+          >
+            {({ x, y }) => (
+              <WorldMap
+                x={x}
+                y={y}
+                height={height}
+                width={width}
+                messageList={messageList}
+              />
+            )}
+          </Motion>
         )}
       </AutoSizer>
     </div>
