205057ffe6ae1c82b69e15a8a3c476f6a90ceb4e
Steve Goldberg <steve.schnee@gmail.com>
2019-03-26 11:27:09 -0700

    Update empty column text

--- src/EmptyColumn.js
+++ src/EmptyColumn.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './EmptyColumn.module.scss'
 
 const EmptyColumn = ({ title }) => (
-  <div className={styles.root}>{title} will display here...</div>
+  <div className={styles.root}>Waiting to receive {title}...</div>
 )
 
 export default EmptyColumn
